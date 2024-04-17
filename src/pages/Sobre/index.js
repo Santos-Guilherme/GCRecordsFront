5bdb36d0fb299c45b44c8bd8f28c86350c3f71c3
Guilherme <santos.guilherme.dev@gmail.com>
2024-04-17 18:47:56 -0300

    Desenvolvendo a página Sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,5 +1,8 @@
 import './index.scss';
-import logo from '../../assets/images/logo.png';
+import imagemlogo from '../../assets/images/logo.png';
+import imagemGrupo from '../../assets/images/sobre-foto-grupo.png';
+import imagemSobre1 from '../../assets/images/sobre-mc-studio.png';
+import imagemSobre2 from '../../assets/images/sobre-crianca-fone.png';
 
 
 import { Link } from 'react-router-dom';
@@ -10,7 +13,7 @@ export default function Sobre() {
         <div className='Sobre'>
             <div className='Content'>
                 <header>
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={imagemlogo} className="App-logo" alt="logo" />
                     <ul>
                         <li className='artista'>
                             <Link to='/Artista'> Artista</Link>
@@ -35,15 +38,15 @@ export default function Sobre() {
                     <h1>Quem somos</h1>
                 </div>
                 <div className='div-paragrafo'>
-                    <img src="" className="App-logo" alt="logo" />
                     <p>Somos uma gravadora brasileira com foco em levar para todas as comunidades o mais alto nivel de musica e cultura.</p>
+                    <img src={imagemGrupo} className="Imagem-Sobre" alt="Imagem do integrantes fundadores e desenvolvedores da GCRecords" />
                 </div>
 
                 <div>
                     <h1>Objetivo</h1>
                 </div>
                 <div className='div-paragrafo'>
-                    <img src="" className="App-logo" alt="logo" />
+                    <img src={imagemSobre1} className="Imagem-Sobre" alt="Imagem de um dos MC's da GCRecords em seu studio" />
                     <p>Nossa missão é proporcionar a excelência da cultura musical a todas as comunidades.</p>
                 </div>
 
@@ -51,10 +54,12 @@ export default function Sobre() {
                     <h1>Impacto Social</h1>
                 </div>
                 <div className='div-paragrafo'>
-                    <img src="" className="App-logo" alt="logo" />
                     <p>G4MP3R Code Records disponibiliza albuns de artistas mundias e nacionais com preços acessiveis para o pessoal da quebrada.</p>
+                    <img src={imagemSobre2} className="Imagem-Sobre" alt="Imagem de uma criança segurando um fone de ouvido na cabeça" />
                 </div>
-
+                <footer>
+                    
+                </footer>
             </div>
         </div>
     )
